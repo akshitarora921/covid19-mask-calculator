88a56c5a622b5846dbcd19bf55b3eaa07b84fde2
CharlieJhonSmith <charlie.alfrido@gmail.com>
2021-10-15 16:15:51 +0000

    add test

---
+++ src/serviceWorker.test.js
@@ -0,0 +1,11 @@
+const serviceWorker = require("./serviceWorker")
+// @ponicode
+describe("serviceWorker.unregister", () => {
+    test("0", () => {
+        let callFunction = () => {
+            serviceWorker.unregister()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
